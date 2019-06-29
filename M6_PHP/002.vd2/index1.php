8cdcb85b5b3cb0da3bc61963507c042ecc2b691c
PHAT <phatnguyen19111997@gmail.com>
2019-06-29 22:02:03 +0700

    phien ban 1

--- M6_PHP/002.vd2/index1.php
+++ M6_PHP/002.vd2/index1.php
@@ -37,5 +37,11 @@
 <?php echo "<br> " ; ?>
 <?php echo '<br>chào "hà nội"'; ?>
 <?php echo "<br>chào \"hà nội\""; ?>
+<pre>
+    Dấu ngoặc đơn ‘ ‘ các bạn nên sử dụng trong trường hợp echo ra một chuỗi bình thường không chưa biến
+    vì tốc độ nó sẽ nhanh hơn là dấu ngoặc kép ” ”
+    vì nó không phải mất thời gian tìm giá trị của biến và parse ra ngoài.
+    Dấu ngoặc kép " " thì ngược lại
+</pre>
 </body>
 </html>
