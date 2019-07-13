983439654894892f3acb45108f73eacd7070ca12
PHAT <phatnguyen19111997@gmail.com>
2019-07-13 22:27:29 +0700

    phien ban 1

--- M6_PHP/009.array/index14.php
+++ M6_PHP/009.array/index14.php
@@ -1,13 +1,17 @@
 <?php
 //mảng bất tuần tự
 $array3 = array();
-$array3["china"] = array("name" => "trung quốc", "city" => array("thượng hải", "bắc kinh", "thâm quyến ", "thành đô "));
+$array3["china"] = array("name" => "trung quốc",
+    "city" => array("thượng hải", "bắc kinh", "thâm quyến ", "thành đô "));
 $array3["japan"] = array("name" => "nhật bản", "city" => array("tokyo") );
-$array3["namdinh"] = array("name" => "nam định", "district" => array("xuân trường", "nam trực", "trực ninh", "nghĩa hưng",
+$array3["namdinh"] = array("name" => "nam định",
+    "district" => array("xuân trường", "nam trực", "trực ninh", "nghĩa hưng",
     "giao thủy", "vụ bản"));
-$array3["vn"] = array("name" => "việt nam", "city" => array("hà nội", "hồ chí minh","nam định", "đồng nai","thái bình") );
+$array3["vn"] = array("name" => "việt nam",
+    "city" => array("hà nội", "hồ chí minh","nam định", "đồng nai","thái bình", "phủ lý") );
 
 //tên các key , value khác nhau
+//mỗi ul 1 quốc gia gồm các thành phố
 echo "<ul>";
 foreach ($array3 as $keyCountry => $Country){
     foreach ($Country as $keyCities => $cities){
