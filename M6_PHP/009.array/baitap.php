cf86569711b07d6d611ba7aed5e0ba974a9adf4e
PHAT <phatnguyen19111997@gmail.com>
2019-07-15 23:14:05 +0700

    phien ban 1

--- M6_PHP/009.array/baitap.php
+++ M6_PHP/009.array/baitap.php
@@ -4,9 +4,6 @@ $array3 = array();
 $array3["china"] = array("name" => "trung quốc",
     "city" => array("thượng hải", "bắc kinh", "thâm quyến ", "thành đô "));
 $array3["japan"] = array("name" => "nhật bản", "city" => array("tokyo") );
-$array3["namdinh"] = array("name" => "nam định",
-    "district" => array("xuân trường", "nam trực", "trực ninh", "nghĩa hưng",
-    "giao thủy", "vụ bản"));
 $array3["vn"] = array("name" => "việt nam",
     "city" => array("hà nội", "hồ chí minh","nam định", "đồng nai","thái bình", "phủ lý") );
 
@@ -15,13 +12,18 @@ $array3["vn"] = array("name" => "việt nam",
 echo "<ul>";
 foreach ($array3 as $keyCountry => $Country){
     foreach ($Country as $keyCities => $cities){
-        if ($keyCities == "city"){
+        if ($keyCities == "name"){
+            echo "<ul>";
+            echo "<li>".$cities;
+
+        }else {
             foreach ($cities as $keyCity => $city) {
-                echo "<li>";
-                echo " " . $city;
-                echo "</li>";
+                echo "<ul>";
+                echo "<li> " . $city;
+                echo "</ul>";
             }
         }
     }
+    echo "</ul>";
 }
 echo "</ul>";
