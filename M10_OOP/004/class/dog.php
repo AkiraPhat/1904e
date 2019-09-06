081dc5a8e69292b67dd9fbf44ffa9c7fc201333c
PHAT <phatnguyen19111997@gmail.com>
2019-09-06 19:25:01 +0700

    phien ban 1

--- M10_OOP/004/class/dog.php
+++ M10_OOP/004/class/dog.php
@@ -1,14 +1,16 @@
 <?php
 
 include_once "../abstract/animals.php";
-
+include_once "../interface/an.php";
+include_once "../interface/keu.php";
+include_once "../interface/sinhsan.php";
 /**
  * Class Cho
  * bất kể class nào khi extend từ abstract class
  * thì phải viết lại code thực thi cho các method
  * abstract của class cha
  */
-class Cho extends Dongvat {
+class Cho extends Dongvat implements An,Keu,Sinhsan {
     public function tenloai()
     {
         echo "This is a dog ";
@@ -17,5 +19,19 @@ class Cho extends Dongvat {
     public function thongtin()
     {
         echo "chó là loài 4 chân chạy nhanh. chó được nuôi làm thú cưng và trông nhà";
+    }
+
+    public function toinayangi(){
+
+    }
+
+    public function keunhuthenao()
+    {
+        // TODO: Implement keunhuthenao() method.
+    }
+
+    public function sinhsannhuthenao()
+    {
+        // TODO: Implement sinhsannhuthenao() method.
     }
 }
